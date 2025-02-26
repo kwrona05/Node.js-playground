aa7c855daf0a05c8731169e7117a5c0cbba0b538
kwrona05 <kwrona05@gmail.com>
2025-02-26 07:28:34 +0100

    Node.js from freecodecamp

---
+++ Lesson 1/app.js
@@ -0,0 +1,9 @@
+//Globals
+
+//__dirname - path to current directory
+//__filename - file name
+//require - function to use modules
+//module - info about current module
+//process - info abourt env where the program is being 
+
+console.log(__dirname)
