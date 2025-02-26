13408b72ec68a2b5f0174b1c8b67c8027218dc55
kwrona05 <kwrona05@gmail.com>
2025-02-26 07:01:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const url = 'http//kacper.com'
+const http = require('http')
 
-const log = (message) => {
-    console.log(message)
-}
+const server = http.createServer((req, res) => {
+    const url = req.url
+    if(url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Welcome</title></head>')
+        res.write('<body><h1>Welcome on my exercise</h1></body>')
+        res.write('</html>')
+    }
+    if(url === '/users') {
+        res.write('<html>')
+        res.write('<head><title>Users</title></head>')
+        res.write('<body><ul><li>User1</li><li>User2</li></ul></body>')
+        res.write('</html>')
+    }
+})
 
-module.exports = log()
+server.listen(3000)
